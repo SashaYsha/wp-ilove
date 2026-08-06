9556210cc8dc35586f79ad097018bedb8648ba95
WP Studio <info@wpstudio.ru>
2026-08-06 08:47:13 +0300

    * remove akismet under git * correct order for installation options

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu pipefail
 PROJECT=ilove
-wp config create --dbname=$PROJECT --dbuser=$PROJECT --dbpass=$PROJECT || true
 wp core download --locale=ru_RU || true
+wp config create --dbname=$PROJECT --dbuser=$PROJECT --dbpass=$PROJECT || true
 wp core install --url=wp-ilove.test --title="WP ilove" --admin_user=dev --admin_password=dev00998877 --admin_email=dev@ilove.test
 wp core version
